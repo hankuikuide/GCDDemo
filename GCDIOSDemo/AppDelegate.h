50f6f415164768bfabea0c08254a557ffa15b21f
tomson <hankuikuide@163.com>
2015-04-15 14:17:41 +0800

    Initial Commit

---
+++ GCDIOSDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GCDIOSDemo
+//
+//  Created by Tomson on 15-4-15.
+//  Copyright (c) 2015年 Org.CTIL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GCDIOSDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GCDIOSDemo
+//
+//  Created by Tomson on 15-4-15.
+//  Copyright (c) 2015年 Org.CTIL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
