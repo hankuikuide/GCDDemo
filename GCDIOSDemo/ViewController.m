ec5e9615efe798a8f3c5ee220773798d32d551f6
tomson <hankuikuide@163.com>
2015-04-19 17:17:32 +0800

    add UIButton and UIImageView

--- GCDIOSDemo/ViewController.m
+++ GCDIOSDemo/ViewController.m
@@ -17,6 +17,19 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 200, 200)];
+
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    
+    button.frame = CGRectMake(0, 200, 200, 100);
+    
+    button.backgroundColor = [UIColor redColor];
+    
+    [self.view addSubview:button];
+    
+    [self.view addSubview:imageView];
+    
 	// Do any additional setup after loading the view, typically from a nib.
     dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
     dispatch_async(queue, ^{
@@ -31,6 +44,19 @@
         NSLog(@"下载图片3-----%@", [NSThread currentThread]);
     });
     
+    dispatch_async(queue, ^{
+        NSURL *url = [NSURL URLWithString:@"http://car0.autoimg.cn/upload/spec/9579/u_20120110174805627264.jpg"];
+        //UIImage *image = [UIImage imageWithData:[NSData dataWithContentsOfURL:url]];
+        NSLog(@"下载图片4-----%@", [NSThread currentThread]);
+        //UIImageView *tmpImage = [[UIImageView alloc] initWithImage:image];
+
+        dispatch_async(dispatch_get_main_queue(), ^{
+            //[imageView insertSubview:tmpImage atIndex:0];
+            imageView.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:url]];
+            NSLog(@"图片4 更新主线程-----%@", [NSThread currentThread]);
+            
+        });
+    });
     NSLog(@"主线程-----%@", [NSThread mainThread]);
 
 }
